90359a8d8eb51baf33131da734605b9ad6b2c856
joexy <josephethethowo@gmail.com>
2024-02-28 13:44:17 +0100

    fixed sideDrawer

--- frontend/src/shared/components/Navigation/SideDrawer.tsx
+++ frontend/src/shared/components/Navigation/SideDrawer.tsx
@@ -1,29 +1,34 @@
-import ReactDOM from 'react-dom';
-import { CSSTransition } from 'react-transition-group'
-import './SideDrawer.css'
+import { useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import './SideDrawer.css';
 
 interface SideDrawerProps {
     show: boolean | undefined;
     children: React.ReactNode;
-    onClick?: React.MouseEventHandler<HTMLElement>; // Add this line
+    onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-const SideDrawer = (props: SideDrawerProps) => {
+const SideDrawer: React.FC<SideDrawerProps> = (props) => {
+    const asideRef = useRef<HTMLDivElement>(null);
 
-    const content =  (
-        <CSSTransition in={props.show} timeout={200} classNames="slide-in-left" mountOnEnter unmountOnExit>
-            <aside className="side-drawer" onClick={props.onClick}> {/* Change this line */}
+    return (
+        <CSSTransition
+            in={props.show}
+            timeout={200}
+            classNames="slide-in-left"
+            mountOnEnter
+            unmountOnExit
+            nodeRef={asideRef} // Add the nodeRef prop
+        >
+            <aside
+                className="side-drawer"
+                onClick={props.onClick}
+                ref={asideRef} // Add the ref to the aside element
+            >
                 {props.children}
             </aside>
         </CSSTransition>
-    )
+    );
+};
 
-    return (
-       ReactDOM.createPortal(
-           content,
-           document.getElementById('drawer-hook') as HTMLElement
-       )
-    )
-}
-
-export default SideDrawer
+export default SideDrawer;
